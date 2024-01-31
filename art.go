44ded92257924487fdc80af6be22bced932ae921
hannes <hannes.study@gmail.com>
2024-01-31 16:56:13 +0200

    added downloaded content files

--- art.go
+++ art.go
@@ -6,10 +6,7 @@ import (
 )
 
 func main() {
-
 	inputArguments := os.Args
-
 	fmt.Println(inputArguments[1])
-
 	fmt.Println("Hello World with art from the ASCII")
 }
---
+++ todo.go
@@ -0,0 +1,19 @@
+package main
+
+//TODO: get input Your. a single command line argument, which will be a string of characters describing the art to be generated.
+//TODO: Decode ABC[10 D]EFG  consutive characters will be palace in [] brackets where the first element will be a number of reps separated by a space followed by the repeating character(s)
+//! The decoder will not make square brackets printable. 
+//TODO write input to file
+//TODO parse input 
+
+//TODO error handling 
+/*
+1)The first argument is not a number
+2)The arguments are not separated by a space
+3)There is no second argument, I.e. empty strings are invalid
+4)Square brackets are unbalanced
+*/
+//TODO  Encode mode **This must be toggled on with a command line argument 
+//TODO  create multiline Decoder ** This must be toggled on with a command line 
+//
+
